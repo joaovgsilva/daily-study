79c96c12075ee498458370bb370e130fa472ccec
João Gomes <joaovgsilva20@gmail.com>
2023-07-15 01:08:50 -0300

    finished-project

---
+++ Dashboard/orders.js
@@ -0,0 +1,32 @@
+const Orders =[
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'LARVENDER K102 Drone',
+        productNumber: '36378',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined' 
+    },
+    {
+        productName: 'Drone with Camera Drone',
+        productNumber: '96996',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'GPS 4k Drone',
+        productNumber: '22821',
+        paymentStatus: 'Paid',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'DJI Air 25',
+        productNumber: '81475',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    }
+]
