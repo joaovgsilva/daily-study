2eb5fb7f5621d8e8219878ff3adaa114ab10bfeb
João Gomes <joaovgsilva20@gmail.com>
2023-08-15 09:52:03 -0300

    finished-contact_section

--- Portfólio/script.js
+++ Portfólio/script.js
@@ -14,3 +14,24 @@ let typed = new Typed('.auto-input',{
     backDelay: 2000,
     loop: true,
 })
+
+// ===== ACTIVE LINK STATE ON SCROLL =====
+
+// ===== GET ALL LINKS =====
+let navLinks = document.querySelectorAll('nav ul li a');
+// ===== GET ALL SECTIONS =====
+let sections = document.querySelectorAll('section');
+
+window.addEventListener('scroll', function (){
+    const scrollPos = window.scrollY + 20;
+    sections.forEach(section => {
+        if(scrollPos > section.offsetTop && scrollPos < (section.offsetTop + section.offsetHeight)){
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+                if(section.getAttribute('id') === link.getAttribute('href').substring(1)){
+                    link.classList.add('active')
+                }
+            });
+        }
+    });
+});
